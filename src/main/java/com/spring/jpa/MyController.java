0fa8ab46e9e92a34172c779eae895de345dcba94
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-01-17 16:46:22 +0530

    add delete function

--- src/main/java/com/spring/jpa/MyController.java
+++ src/main/java/com/spring/jpa/MyController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
@@ -15,7 +19,7 @@ public class MyController {
 	@Autowired
 	ProductService service;
 	
-	@RequestMapping("/show")
+	@RequestMapping("/")
 	public String show(Model model) {
 		
 		List<Product> list = service.displayProduct();
@@ -24,5 +28,28 @@ public class MyController {
 		}
 		model.addAttribute("data", list);
 		return "index";
+	}
+	@RequestMapping("/new")
+	public String addNewData(Model model) {
+		Product product = new Product();  // model class is product class
+		model.addAttribute("product", product);
+		return "new_Product";
+	}
+	@RequestMapping(value="/add", method = RequestMethod.POST)
+	public String addData(@ModelAttribute("product")Product product) {
+		service.save(product);
+		return "redirect:/";
+	}
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditProductPage(@PathVariable(name="id") int id) {
+		ModelAndView view = new ModelAndView("edit_product");
+		Product product = service.get(id);
+		view.addObject("product", product);
+		return view;
+	}
+	@RequestMapping("/delete/{id}")
+	public String deleteProductPage(@PathVariable(name="id") int id) {
+		service.delete(id);
+		return "redirect:/";
 	}
 }
